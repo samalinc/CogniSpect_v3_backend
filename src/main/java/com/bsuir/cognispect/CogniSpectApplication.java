a1e0bb742af19ea3ede589034d502e957a951c0b
Yauheni Harkavik <bugatti1701@gmail.com>
2019-03-14 23:40:28 +0300

    Initialized project

---
+++ src/main/java/com/bsuir/cognispect/CogniSpectApplication.java
@@ -0,0 +1,13 @@
+package com.bsuir.cognispect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CogniSpectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CogniSpectApplication.class, args);
+    }
+
+}
