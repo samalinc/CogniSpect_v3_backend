e67e56f3a2622b1c881f869e444aaa4a520456f0
Yauheni Harkavik <bugatti1701@gmail.com>
2019-03-18 01:19:46 +0300

    Added data.sql file for initialize database and dto for signUp

---
+++ src/main/java/com/bsuir/cognispect/dto/SignUpDto.java
@@ -0,0 +1,33 @@
+package com.bsuir.cognispect.dto;
+
+import com.bsuir.cognispect.entity.enums.RoleEnum;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class SignUpDto {
+    @NotBlank
+    @Size(min = 3, max = 15)
+    private String login;
+
+    @NotBlank
+    @Size(max = 40)
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(min = 6, max = 20)
+    private String password;
+
+    private RoleEnum role;
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO roles(id, role_name) VALUES('d8c9ed00-41ec-4ebd-aed2-3446ed2c4b6f', 'ADMIN');
+INSERT INTO roles(id, role_name) VALUES('22fdca31-b67a-49f1-b457-c34c669d3b18', 'STUDENT');
+INSERT INTO roles(id, role_name) VALUES('3c037ad1-5dcf-4d88-87d5-7b0f8a3d90b3', 'TEACHER');
