5c9bcc041569b24307437a674faf6ebfd2e7db10
Yauheni Harkavik <bugatti1701@gmail.com>
2019-04-14 11:51:08 +0300

    Added logger for socket server

--- src/main/java/com/bsuir/cognispect/socket/SocketServer.java
+++ src/main/java/com/bsuir/cognispect/socket/SocketServer.java
@@ -2,9 +2,10 @@ package com.bsuir.cognispect.socket;
 
 import com.corundumstudio.socketio.Configuration;
 import com.corundumstudio.socketio.SocketIOServer;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
-
+@Slf4j
 @Component
 public class SocketServer {
 
@@ -18,11 +19,16 @@ public class SocketServer {
         socketIOServer = new SocketIOServer(config);
 
         setupListeners();
+        log.info("Start SocketIO server");
         socketIOServer.start();
     }
 
     private void setupListeners() {
+        socketIOServer.addConnectListener((socketIOClient) -> {
+            log.info(socketIOClient.toString() + "is connected");
+        });
         socketIOServer.addEventListener("new_message", Object.class, (socketIOClient, someObject, ackRequest) -> {
+            log.info("Event: new_message triggered by " + socketIOClient.toString());
             if (ackRequest.isAckRequested()) {
                 ackRequest.sendAckData(someObject.toString());
             }
