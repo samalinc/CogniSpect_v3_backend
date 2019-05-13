59f2ff573d317f1ad43e65eb48c5f7c259605e6b
Yauheni Harkavik <bugatti1701@gmail.com>
2019-05-13 18:00:31 +0300

    init script added

---
+++ src/main/resources/init_script.sql
@@ -0,0 +1,23 @@
+INSERT INTO account
+VALUES ('4e349d3c-758e-11e9-8f9e-2a86e4085a59', 'y.harkavik@gmail.com',
+        '$2y$12$Gz/SiWB2whg4xApG1KREAO9mfbFB0woH5OBIAzEXfx3u2s4EcIi0e',
+        '6505027', 'ADMIN');
+INSERT INTO account
+VALUES ('4e349fd0-758e-11e9-8f9e-2a86e4085a59', 'mairyn@gmail.com',
+        '$2y$12$sC33Vt6FsCsUdc6bIl0OkeZOwi/2BZtFXPYg1zKavUMk1tMRyi5fa',
+        '6505057', 'STUDENT');
+INSERT INTO account
+VALUES ('4e34a3e0-758e-11e9-8f9e-2a86e4085a59', 'samal@bsuir.by',
+        '$2y$12$N0N/PzSLpW6j0FAGQG1BoOQuuTUU3/uRlzOV20mYPmGDvrNOi6DAi',
+        'samal', 'TEACHER');
+
+INSERT INTO teacher
+VALUES ('285ec9ba-758f-11e9-8f9e-2a86e4085a59', 'Yauheni', 'Harkavik',
+        '4e349d3c-758e-11e9-8f9e-2a86e4085a59');
+INSERT INTO student
+VALUES ('285ecc3a-758f-11e9-8f9e-2a86e4085a59', 'Alexey', 'Mairyn', '650505',
+        '4e349fd0-758e-11e9-8f9e-2a86e4085a59');
+INSERT INTO teacher
+VALUES ('285ecd8e-758f-11e9-8f9e-2a86e4085a59', 'Dmitriy', 'Samal',
+        '4e34a3e0-758e-11e9-8f9e-2a86e4085a59');
+
